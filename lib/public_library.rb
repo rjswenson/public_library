620f5bb9d1a44bdedc02f02f5c7ea4effac6e746
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-09 16:25:25 -0700

    books includes library arg, unshelved method added for errant books

--- lib/public_library.rb
+++ lib/public_library.rb
@@ -1,24 +1,27 @@
-
-
 class Book
   attr_reader :title 
   
-  def initialize(title)
+  def initialize(title, library)
     @title = title
+    @library = library
+    @library.unshelved(@title, true)
+
   end
 
   def enshelf(shelf)
-    if shelf.books_on_shelf == nil || shelf.books_on_shelf.include?(self) == false      #attaches a book to a shelf if not already done so
+    if shelf.books == nil || shelf.books.include?(self) == false      #attaches a book to a shelf if not already done so
       @shelf = shelf
       shelf.add_book(self)     #adds instance of Book (ie self) to the shelf class instance passed in as an argument
+      @library.unshelved(@title, false)
     else
       raise "The book is already on a shelf!"      #raises exception if book is already on a shelf
     end
   end
 
   def unshelf
-    if @shelf.books_on_shelf != nil && @shelf.books_on_shelf.include?(self) == true     #removes book if it is indeed on a shelf
+    if @shelf.books != nil && @shelf.books.include?(self) == true     #removes book if it is indeed on a shelf
       @shelf.remove_book(self)
+      @library.unshelved(@title, true)
     else
       raise "That book is not on a shelf!"
     end      
@@ -27,58 +30,53 @@ end
 
 
 class Shelf
-  attr_reader :books_on_shelf   #allows us to see what books are on a particular class instance of Shelf
+  attr_reader :books   #allows us to see what books are on a particular class instance of Shelf
 
   def initialize (library)
     library.add_shelf(self)   #as shelf is initialized, it adds itself to the shelves within the library of which it is a member
-    @books_on_shelf = []
+    @books = []
   end
 
   def remove_book(book)
-    @books_on_shelf.delete(book)
+    @books.delete(book)
   end
 
   def add_book(book)
-    @books_on_shelf.push(book) 
+    @books.push(book) 
   end
 end
 
 
 class Library
-  attr_reader :all_shelves
+  attr_reader :shelves
   
   def initialize
-    @all_shelves = []
+    @shelves = []
     @all_books = []
+    @unshelved_books = []
+    @shelved_books = []
   end
 
   def add_shelf(shelf)           #this method is actually run by an instance of class Shelf, to push into the Library instance
-    @all_shelves.push(shelf)
+    @shelves.push(shelf)
+  end
+
+  def shelved
+    @shelves.each do |x|                                   #for each shelf
+      x.books.each do |y|                                  #examine each book on said shelf
+        @shelved_books = @shelved_books.push(y.title)      #and push its title to the @all_books array
+      end
+    end
+    @shelved_books
+  end
+  
+  def unshelved(book, bool)                                #called by instance of class Book
+    @unshelved_books.push(book) if bool == true            #keeps track of books removed from shelves
+    @unshelved_books.delete(book) if bool == false
   end
 
   def all_books
-    @all_shelves.each do |x|              #for each shelf
-      x.books_on_shelf.each do |y|        #examine each book on said shelf
-        @all_books.push(y.title)          #and push its title to the @all_books array
-      end
-    end
-    
-    @all_books
+   @all_books = @shelved_books | @unshelved_books          #adds all the books together
+   @all_books.flatten.sort                                 #sorts alphabetically
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- spec/public_library_spec.rb
+++ spec/public_library_spec.rb
@@ -1,6 +1,5 @@
 require_relative '../lib/public_library.rb'
 
-
 describe 'Book(class)' do 
   it 'exists' do
     expect(Book).to be
@@ -9,37 +8,38 @@ end
 
 describe '#title within Book' do
   it 'stores a #title' do
-    test_book = Book.new("bar")
+    lib = Library.new
+    test_book = Book.new("bar", lib)
     expect(test_book.title).to eq("bar")
   end
 end
 
 describe '#enshelf within Book' do
   it 'puts a book on a shelf' do
-    test_book = Book.new("Magic")
     library_ex = Library.new
+    test_book = Book.new("Magic", library_ex)
     sample_shelf = Shelf.new(library_ex)
     test_book.enshelf(sample_shelf)
-    expect(sample_shelf.books_on_shelf).to eq([test_book])
+    expect(sample_shelf.books).to eq([test_book])
   end
 end
 
 describe '#unshelf within Book' do
   it 'removes shelf from Book properties' do
-    test_book = Book.new("Sweet Book")
     library_ex = Library.new
+    test_book = Book.new("Sweet Book", library_ex)
     sample_shelf = Shelf.new(library_ex)
     test_book.enshelf(sample_shelf)
     test_book.unshelf
-    expect(sample_shelf.books_on_shelf).to_not include(test_book)
+    expect(sample_shelf.books).to_not include(test_book)
   end
 end
 
 
 describe '#enshelf' do
   it 'wont allow book to be put on multi shelves' do
-    test_book = Book.new("book")
     library_ex = Library.new
+    test_book = Book.new("book", library_ex)
     shelf_a = Shelf.new(library_ex)
     shelf_b = Shelf.new(library_ex)
     test_book.enshelf(shelf_a)
@@ -50,43 +50,43 @@ end
 
 describe '#enshelfing more then one book on the same shelf' do 
   it 'shows more then one book on the shelf' do
-    book_a = Book.new("Meyow")
-    book_b = Book.new("Mix")
     library_ex = Library.new
+    book_a = Book.new("Meyow", library_ex)
+    book_b = Book.new("Mix", library_ex)
     shelf_a = Shelf.new(library_ex)
     book_a.enshelf(shelf_a)
     book_b.enshelf(shelf_a)
-    expect(shelf_a.books_on_shelf).to eq([book_a, book_b])
+    expect(shelf_a.books).to eq([book_a, book_b])
   end
 end
     
 describe '#unshelf removes book from shelf' do
   it 'removes a book already on shelf' do
-    book_a = Book.new("Test")
     library_ex = Library.new
+    book_a = Book.new("Test", library_ex)
     shelf_a = Shelf.new(library_ex)
     book_a.enshelf(shelf_a)
     book_a.unshelf
-    expect(shelf_a.books_on_shelf).not_to include(book_a)
+    expect(shelf_a.books).not_to include(book_a)
   end
 
   it 'removes a book from a shelf, after others have been added' do
-    book_a = Book.new("Test")
-    book_b = Book.new("Spongebob")
     library_ex = Library.new
+    book_a = Book.new("Test", library_ex)
+    book_b = Book.new("Spongebob", library_ex)
     shelf_a = Shelf.new(library_ex)
     book_a.enshelf(shelf_a)
     book_b.enshelf(shelf_a)
     book_a.unshelf
-    expect(shelf_a.books_on_shelf).not_to include(book_a)
+    expect(shelf_a.books).not_to include(book_a)
   end
 
   it 'can handle multiple shelf changes' do
-    book_a = Book.new("Test")
-    book_b = Book.new("Spongebob")
-    book_c = Book.new("Something")
-    book_d = Book.new("Game of Thrones")
     library_ex = Library.new
+    book_a = Book.new("Test", library_ex)
+    book_b = Book.new("Spongebob", library_ex)
+    book_c = Book.new("Something", library_ex)
+    book_d = Book.new("Game of Thrones", library_ex)
     shelf_a = Shelf.new(library_ex)
 
     book_a.enshelf(shelf_a)
@@ -96,23 +96,22 @@ describe '#unshelf removes book from shelf' do
     book_a.enshelf(shelf_a)
     book_b.unshelf
     book_d.enshelf(shelf_a)
-    expect(shelf_a.books_on_shelf).to include(book_a, book_c, book_d)
-    expect(shelf_a.books_on_shelf).to_not include(book_b)
+    expect(shelf_a.books).to include(book_a, book_c, book_d)
+    expect(shelf_a.books).to_not include(book_b)
   end
 end
 
 describe 'Library' do 
   it 'explicitly stores shelves' do
     lib = Library.new
-    library_ex = Library.new
-    shelf_a = Shelf.new(library_ex)
+    shelf_a = Shelf.new(lib)
     expect(lib.add_shelf(shelf_a)).to include(shelf_a)
   end
   it 'stores multiple shelves' do
     library_ex = Library.new
     shelf_b = Shelf.new(library_ex)
     shelf_a = Shelf.new(library_ex)
-    expect(library_ex.all_shelves).to include(shelf_b, shelf_a)
+    expect(library_ex.shelves).to include(shelf_b, shelf_a)
   end
   it 'displays all the shelves' do
     library_ex = Library.new
@@ -124,30 +123,65 @@ describe 'Library' do
   it 'displays all the books on one shelf' do
     library_ex = Library.new
     a_f_shelf = Shelf.new(library_ex)
-    a_book = Book.new("Allibaster")
-    c_book = Book.new("Cookies!")
+    a_book = Book.new("Allibaster", library_ex)
+    c_book = Book.new("Cookies!", library_ex)
     a_book.enshelf(a_f_shelf)
     c_book.enshelf(a_f_shelf)
-    expect(library_ex.all_books).to eq(["Allibaster", "Cookies!"])
+    expect(library_ex.shelved).to eq(["Allibaster", "Cookies!"])
   end
 
   it 'displays all the books on 2 shelves' do
     library_ex = Library.new
     a_f_shelf = Shelf.new(library_ex)
     g_z_shelf = Shelf.new(library_ex)
-    f_book = Book.new("Frankenstein")
-    z_book = Book.new("Zebras")
+    f_book = Book.new("Frankenstein", library_ex)
+    z_book = Book.new("Zebras", library_ex)
     f_book.enshelf(a_f_shelf)
     z_book.enshelf(g_z_shelf)
-    expect(library_ex.all_books).to eq(["Frankenstein", "Zebras"])
+    expect(library_ex.shelved).to eq(["Frankenstein", "Zebras"])
   end
 
   it 'is aware of all the shelves within it' do
     library_ex = Library.new
     shelf_a = Shelf.new(library_ex)
     shelf_b = Shelf.new(library_ex)
-    expect(library_ex.all_shelves). to include(shelf_a, shelf_b)
+    expect(library_ex.shelves). to include(shelf_a, shelf_b)
   end
 end
+
+describe 'Shows total books' do
+=begin it 'includes only nonshelved books' do
+    lib = Library.new
+    book_a = Book.new("Jolly", lib)
+    book_b = Book.new("Roger", lib)
+    expect(lib.all_books).to eq(["Jolly", "Roger"])
+  end
+  it 'includes only shelved books' do
+    lib = Library.new
+    book_a = Book.new("Jolly", lib)
+    book_b = Book.new("Roger", lib)
+    shelf = Shelf.new(lib)
+    book_a.enshelf(shelf)
+    book_b.enshelf(shelf)
+
+    expect(lib.all_books).to eq(["Jolly", "Roger"])
+=end
+  it 'includes non and shelved books' do
+    lib = Library.new
+    book_a = Book.new("Jolly", lib)
+    shelf = Shelf.new(lib)
+    book_a.enshelf(shelf)
+    book_b = Book.new("Apple", lib)
+    book_c = Book.new("Ex", lib)
+    book_c.enshelf(shelf)
+    lib.shelved
+    expect(lib.all_books).to eq(["Apple", "Ex", "Jolly"])
+  end
+end
+
+
+
+
+
 
 
