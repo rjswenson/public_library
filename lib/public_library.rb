b1548e16f340e6c05de1bf606a8c397d515668c5
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-09 10:11:46 -0700

    modded #enshelf to push book to shelf completely

--- lib/public_library.rb
+++ lib/public_library.rb
@@ -3,13 +3,18 @@
 class Book
   
   attr_reader :shelf , :title
-
+  @@shelf_counter = 0
   def initialize(title)
     @title = title
   end
 
   def enshelf(shelf)
-    @shelf = shelf
+    @@shelf_counter += 1
+    if @@shelf_counter > 1
+      "The book is already on a shelf!"
+    else
+      shelf.add_book(self)
+    end
   end
 
   def unshelf
@@ -19,5 +24,13 @@ end
 
 
 class Shelf
+  attr_reader :books_on_shelf
 
+  def initialize
+  
+  end
+
+  def add_book(book)
+    @books_on_shelf = [book]
+  end
 end
--- spec/public_library_spec.rb
+++ spec/public_library_spec.rb
@@ -19,7 +19,7 @@ describe '#enshelf within Book' do
     test_book = Book.new("Magic")
     sample_shelf = Shelf.new
     test_book.enshelf(sample_shelf)
-    expect(test_book.shelf).to eq(sample_shelf)
+    expect(sample_shelf.books_on_shelf).to eq([test_book])
   end
 end
 
@@ -32,6 +32,16 @@ describe '#unshelf within Book' do
   end
 end
 
+
+describe '#enshelf' do
+  it 'wont allow book to be put on multi shelves' do
+    test_book = Book.new("book")
+    shelf_a = Shelf.new
+    shelf_b = Shelf.new
+    test_book.enshelf(shelf_a)
+    expect(test_book.enshelf(shelf_b)).to eq("The book is already on a shelf!")
+  end
+end
 
     
 
